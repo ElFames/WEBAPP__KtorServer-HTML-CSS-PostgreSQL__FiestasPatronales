a56358442578bbc6d3ec0cc7767c9f4b1ff64ece
walid <walid.abdullah.7e5@itb.cat>
2022-12-04 00:40:52 +0100

    home reference update and feast post method

--- src/main/kotlin/wm/routes/fiestasPatronalesRouting.kt
+++ src/main/kotlin/wm/routes/fiestasPatronalesRouting.kt
@@ -1,18 +1,24 @@
 package wm.routes
 
+import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.html.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.jetbrains.exposed.sql.statements.api.ExposedBlob
 import wm.data.FeastDAO
 import wm.templates.LayoutTemplate
 
 fun Route.fiestasPatronalesRouting() {
     val feastDAO = FeastDAO()
+    feastDAO.connect()
 
 
     route("/fiestaspatronales") {
 
-        get("home") {
+        get {
             call.respondHtmlTemplate(LayoutTemplate(feastDAO)) {
                 this.content = "home"
             }
@@ -41,6 +47,37 @@ fun Route.fiestasPatronalesRouting() {
             call.respondHtmlTemplate(LayoutTemplate(feastDAO)) {
                 this.content = "api"
             }
+        }
+        post("/newFeast") {
+            val partData = call.receiveMultipart()
+            var name = ""
+            var dates = ""
+            var city = ""
+            var town = ""
+            var likes = 0
+            var description: String = ""
+            var image: ByteArray? = null
+            partData.forEachPart { part ->
+                when (part) {
+                    is PartData.FormItem -> {
+                        when (part.name){
+                            "name" -> name = part.value
+                            "dates" -> dates = part.value
+                            "city" -> city = part.value
+                            "town" -> town = part.value
+                            "likes" -> likes = part.value.toInt()
+                            else -> description = part.value
+                        }
+                    }
+                    is PartData.FileItem -> {
+                        image = part.streamProvider().readBytes()
+                    }
+                    else -> {}
+                }
+                part.dispose()
+            }
+            feastDAO.addFeast(name, dates, city, town, likes, image, description)
+            call.respondText("Film storage correctly", status = HttpStatusCode.Created)
         }
     }
 }
