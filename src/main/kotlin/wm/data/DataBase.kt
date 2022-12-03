9be2f30a709d0a1665789df9ff2841f2809b453b
walid <walid.abdullah.7e5@itb.cat>
2022-12-03 19:24:45 +0100

    insert to database functions added

--- src/main/kotlin/wm/data/DataBase.kt
+++ src/main/kotlin/wm/data/DataBase.kt
@@ -2,10 +2,9 @@ package wm.data
 
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
-import wm.models.Citys
-import wm.models.Feasts
-import wm.models.Towns
+import wm.models.*
 
 object DataBase {
     fun init() {
@@ -16,4 +15,43 @@ object DataBase {
             SchemaUtils.create(Citys, Towns,Feasts)
         }
     }
+    fun addFeast(feast: Feast) {
+        transaction {
+            Feasts.insert {
+                it[city] = feast.city
+                it[town] = feast.town
+                it[name] = feast.name
+                it[dates] = feast.dates
+                it[likes] = feast.likes
+                it[image] = feast.image
+                it[description] = feast.description
+            }
+        }
+    }
+    fun addCity(city: City) {
+        transaction {
+            Citys.insert {
+                it[name] = city.name
+            }
+        }
+    }
+    fun addTown(town: Town) {
+        transaction {
+            Towns.insert {
+                it[name] = town.name
+                it[city] = town.city
+            }
+        }
+    }
+    fun addUser(user: User) {
+        transaction {
+            Users.insert {
+                it[name] = user.name
+                it[password] = user.password
+                it[email] = user.email
+                it[role] = user.role
+            }
+        }
+    }
+
 }
