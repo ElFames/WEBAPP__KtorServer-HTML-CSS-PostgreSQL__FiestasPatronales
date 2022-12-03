e0488ecacbafee04ed58629ee791c5231bcd4a02
walid <walid.abdullah.7e5@itb.cat>
2022-12-03 19:25:48 +0100

    insert to database functions added

--- src/main/kotlin/wm/data/FeastDAO.kt
+++ src/main/kotlin/wm/data/FeastDAO.kt
@@ -1,6 +1,43 @@
 package wm.data
 
-import org.jetbrains.exposed.sql.transactions.transaction
+import wm.models.City
+import wm.models.Feast
+import wm.models.Town
+import wm.models.User
 
 class FeastDAO {
+    fun addFeast(name: String, city: String, town: String, dates: String, likes: Int, image: String, description: String) {
+        val feast = Feast.new {
+            this.name = name
+            this.city = city
+            this.town = town
+            this.dates = dates
+            this.likes = likes
+            this.image = image
+            this.description = description
+        }
+        DataBase.addFeast(feast)
+    }
+    fun addCity(name: String) {
+        val city = City.new {
+            this.name = name
+        }
+        DataBase.addCity(city)
+    }
+    fun addTown(name: String, city: String) {
+        val town = Town.new {
+            this.name = name
+            this.city = city
+        }
+        DataBase.addTown(town)
+    }
+    fun addUser(name: String, password: String, email: String, role: String) {
+        val user = User.new {
+            this.name = name
+            this.password = password
+            this.email = email
+            this.role = role
+        }
+        DataBase.addUser(user)
+    }
 }
