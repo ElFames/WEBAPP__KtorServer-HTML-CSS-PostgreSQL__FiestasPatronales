e7eaf3b6acc2cc5b4eee1fd3d2a49acb394f59d6
walid <walid.abdullah.7e5@itb.cat>
2022-12-03 19:57:20 +0100

    possible end points

--- src/main/kotlin/wm/templates/ApiTemplate.kt
+++ src/main/kotlin/wm/templates/ApiTemplate.kt
@@ -2,7 +2,9 @@ package wm.templates
 
 import io.ktor.server.html.*
 import kotlinx.html.FlowContent
+import kotlinx.html.div
 import kotlinx.html.h1
+import kotlinx.html.p
 import wm.data.FeastDAO
 
 data class ApiTemplate(val feastDAO: FeastDAO) : Template<FlowContent> {
@@ -10,6 +12,19 @@ data class ApiTemplate(val feastDAO: FeastDAO) : Template<FlowContent> {
     override fun FlowContent.apply() {
         h1 {
             +"Api endpoints"
+        }
+        div {
+            p { +"GET /api/fiestaspatronales" }
+            p { +"GET /api/fiestaspatronales/{id}" }
+            p { +"GET /api/fiestaspatronales/{id}/likes" }
+            p { +"GET /api/fiestaspatronales/{id}/comments" }
+            p { +"GET /api/fiestaspatronales/{id}/images" }
+            p { +"POST /api/fiestaspatronales/{id}/likes" }
+            p { +"POST /api/fiestaspatronales/{id}/comments" }
+            p { +"POST /api/fiestaspatronales/add_feast" }
+            p { +"POST /api/fiestaspatronales/add_city" }
+            p { +"POST /api/fiestaspatronales/add_town" }
+            p { +"POST /api/fiestaspatronales/add_user" }
         }
     }
 }
