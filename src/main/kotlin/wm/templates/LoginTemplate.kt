6f573b9d159f320b658c0fdb1cd4d61febeacf1f
m <m>
2022-12-04 21:23:27 +0100

    add style to login

--- src/main/kotlin/wm/templates/LoginTemplate.kt
+++ src/main/kotlin/wm/templates/LoginTemplate.kt
@@ -10,43 +10,51 @@ class LoginTemplate: Template<HTML> {
         head {
             link(rel = "icon", href = "/static/logo.png", type="image/png")
             link(rel = "stylesheet", href = "/static/loginStyle.css", type = "text/css")
+            link(rel = "stylesheet", href = "/static/style.css", type = "text/css")
         }
         body {
-            div {
-                form {
-                    action = "/fiestaspatronales/checkLogin"
-                    encType = FormEncType.textPlain
-                    method = FormMethod.post
+            div("login") {
+                div("mainbox") {
+                    img {
+                        src = "/static/logo.png"
+                        alt = "logo"
+                    }
+                    form {
+                        action = "/fiestaspatronales/checkLogin"
+                        encType = FormEncType.textPlain
+                        method = FormMethod.post
 
-                    input {
-                        id = "nickname"
-                        name = "nickname"
-                        type = InputType.text
-                        placeholder = "Nombre de usuario"
-                        required = true
-                    }
-                    br {}
-                    input {
-                        id = "password"
-                        name = "password"
-                        type = InputType.password
-                        placeholder = "Contraseña"
-                        required = true
-                    }
-                    br {}
+                        input {
+                            id = "nickname"
+                            name = "nickname"
+                            type = InputType.text
+                            placeholder = "Usuario"
+                            required = true
+                        }
+                        br {}
+                        input {
+                            id = "password"
+                            name = "password"
+                            type = InputType.password
+                            placeholder = "Contraseña"
+                            required = true
+                        }
+                        br {}
 
-                    input(classes = "button"){
-                        type = InputType.submit
-                        value = "Login"
-                        attributes["aria-selected"] = "true"
-                    }
-                    input(classes = "button"){
-                        formAction = "/fiestaspatronales/newUser"
-                        type = InputType.submit
-                        value = "Registrar"
-                        attributes["aria-selected"] = "true"
+                        input(classes = "button"){
+                            type = InputType.submit
+                            value = "Login"
+                            attributes["aria-selected"] = "true"
+                        }
+                        input(classes = "button"){
+                            formAction = "/fiestaspatronales/newUser"
+                            type = InputType.submit
+                            value = "Registrar"
+                            attributes["aria-selected"] = "true"
+                        }
                     }
                 }
+
             }
         }
     }
