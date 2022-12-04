02c33558d07f454ad40cac918e6886db4c809976
m <m>
2022-12-04 04:16:36 +0100

    login, registro, subir fiestas, formulario de contacto, mapa de google añadido. el back hecho y comprobado, el front medio echo

--- src/main/kotlin/wm/data/DataBase.kt
+++ src/main/kotlin/wm/data/DataBase.kt
@@ -12,7 +12,7 @@ object DataBase {
     fun init() {
         val driver = "org.postgresql.Driver"
         val dbUrl = "jdbc:postgresql://localhost:5432/postgres"
-        val database = Database.connect(dbUrl, driver, user="postgres", password="fames")
+        val database = Database.connect(dbUrl, driver, user="postgres", password="")
         transaction(database) {
             SchemaUtils.create(Citys,Towns,Feasts,Users)
         }
